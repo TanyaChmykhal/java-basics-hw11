c567011654f274bbe5f63fb88ce2e43b5f84482b
TanyaChmykhal <tanya_chmykhal@ukr.net>
2020-05-03 00:43:36 +0300

    commit the home work Lambda

--- src/Lambda.java
+++ src/Lambda.java
@@ -2,24 +2,11 @@ import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class Lambda {
-
-    /**
-     * Function to get max value.
-     *
-     * @return lambda expression.
-     */
     public static BiFunction<Double, Double, Double> getMax() {
-        // TODO replace null with lambda expression
-        return null;
+        return (x, y) -> Math.max(x, y);
     }
 
-    /**
-     * Function to get square root.
-     *
-     * @return lambda expression.
-     */
     public static Function<Double, Double> getSqrt() {
-        // TODO replace null with method reference
-        return null;
+        return Math::sqrt;
     }
 }
